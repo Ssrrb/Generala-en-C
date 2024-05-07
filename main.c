4929cc46992eec977314b2ab1d71ea4ce9e18485
Sebastian <125369302+Ssrrb@users.noreply.github.com>
2024-05-06 21:27:30 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 //Comando para ejecutar gcc -o GeneralaJuego .\Generala.c .\Generala.h .\main.c
-
+/*************************************************************************
+ * Programado por: Sebastian Rojas y Mateo Rios                          *
+ * Clase: TAI                                                            *
+ * Tarea: Juego de generala                                              *
+ * Fecha: 5/06/24                                                        *
+ ************************************************************************/
 #include "Generala.h"
 
 void main (void)
